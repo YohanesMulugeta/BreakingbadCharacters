da0f04c9922ea5a401c2f3cfaf6e0a3c48e630f3
Yohanes <yohanesmulugeta21@gmail.com>
2022-07-08 09:52:23 +0300

    boiler plate for characterlist, navBar, and SearchBar components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+import NavBar from "./component/NavBar";
+import SearchBar from "./component/SearchBar";
+import CharacterList from "./component/CharacterList";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <NavBar />
+      <SearchBar />
+      <CharacterList />
+    </>
   );
-}
+};
 
 export default App;
